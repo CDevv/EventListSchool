9875883cfd5e2bac7af292d612ec186a96d336cd
Kaloyan Ivanov <cdevyt@gmail.com>
2022-05-04 17:07:55 +0300

    Add DataTimePicker

--- App.js
+++ App.js
@@ -10,16 +10,18 @@ const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName='EventList'>
-        <Stack.Screen
-          name='EventList' component={EventList} options={{title: 'Events'}}
-        ></Stack.Screen>
-        <Stack.Screen
-          name='EventForm' component={EventForm} options={{title: 'Add event'}}
-        ></Stack.Screen>
-      </Stack.Navigator>
-    </NavigationContainer>
+    <View>
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName='EventForm'>
+          <Stack.Screen
+            name='EventForm' component={EventForm} options={{ title: 'Add event' }}
+          ></Stack.Screen>
+          <Stack.Screen
+            name='EventList' component={EventList} options={{ title: 'Events' }}
+          ></Stack.Screen>
+        </Stack.Navigator>
+      </NavigationContainer>
+    </View>
   );
 }
 /*
--- EventForm.js
+++ EventForm.js
@@ -1,28 +1,59 @@
 import React, { Component } from 'react';
 import { View, Button, TextInput, StyleSheet } from 'react-native';
+import DateTimePicker from '@react-native-community/datetimepicker';
+import { formatDate } from './util.js';
 
 class EventForm extends Component {
     state = {
-        title: ''
+        title: '',
+        date: Date.now(),
+        showDatePicker: false
     }
 
     handleChangeTitle = (value) => {
         this.setState({ title: value });
     }
 
+    handleDatePress = () => {
+        this.setState({showDatePicker: true});
+    }
+
+    handleDatePickerHide = () => {
+        this.setState({showDatePicker: false});
+    }
+
+    handleDatePicked = (event, selectedDate) => {
+        const currentDate = selectedDate || this.state.date;
+        this.state.date = currentDate;
+        this.handleDatePickerHide();
+    }
+
     render() {
         return (
             <View>
-                <Button
-                    title='Add'
-                    onPress={() => {this.props.navigation.goBack();}}
-                ></Button>
                 <TextInput
                     style={styles.text}
                     placeholder='Event title'
                     value={this.state.title}
                     onChangeText={this.handleChangeTitle}
                 />
+                <TextInput
+                    style={styles.text}
+                    placeholder='Event date'
+                    value={formatDate(this.state.date)}
+                    editable={!this.state.showDatePicker}
+                    onFocus={this.handleDatePicked}
+                />
+                {this.state.showDatePicker && <DateTimePicker
+                    value={this.state.date}
+                    mode='datetime'
+                    is24Hour={true}
+                    onChange={this.handleDatePicked}
+                />}
+                <Button
+                    title='Add'
+                    onPress={() => {this.props.navigation.goBack();}}
+                ></Button>
             </View>
         )
     }
